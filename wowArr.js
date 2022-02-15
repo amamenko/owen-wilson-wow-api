001823958978991382fa1148828a5381043489a4
amamenko <amamenko@hotmail.com>
2022-02-14 22:39:27 -0500

    First commit

---
+++ wowArr.js
@@ -0,0 +1,139 @@
+const wowArr = [
+  {
+    movie: "Bottlerocket",
+    year: 1996,
+    character: "Dignan",
+  },
+  {
+    movie: "Breakfast of Champions",
+    year: 1999,
+    character: "Monte Rapid",
+  },
+  {
+    movie: "The Haunting",
+    year: 1999,
+    character: "Luke Sanderson",
+  },
+  {
+    movie: "Shanghai Noon",
+    year: 2000,
+    character: "Roy O'Bannon",
+  },
+  {
+    movie: "Meet the Parents",
+    year: 2000,
+    character: "Kevin Rawley",
+  },
+  {
+    movie: "Zoolander",
+    year: 2001,
+    character: "Hansel McDonald",
+  },
+  {
+    movie: "I Spy",
+    year: 2002,
+    character: "Alexander Scott",
+  },
+  {
+    movie: "Shanghai Knights",
+    year: 2003,
+    character: "Roy O'Bannon",
+  },
+  {
+    movie: "The Big Bounce",
+    year: 2004,
+    character: "Jack Ryan",
+  },
+  {
+    movie: "Starsky & Hutch",
+    year: 2004,
+    character: "Ken Hutchinson",
+  },
+  {
+    movie: "Wedding Crashers",
+    year: 2005,
+    character: "John Beckwith",
+  },
+  {
+    movie: "Cars",
+    year: 2006,
+    character: "Lightning McQueen",
+  },
+  {
+    movie: "You, Me and Dupree",
+    year: 2006,
+    character: "Dupree",
+  },
+  {
+    movie: "The Darjeeling Limited",
+    year: 2007,
+    character: "Francis",
+  },
+  {
+    movie: "Drillbit Taylor",
+    year: 2008,
+    character: "Drillbit Taylor",
+  },
+  {
+    movie: "Marley & Me",
+    year: 2008,
+    character: "John Grogan",
+  },
+  {
+    movie: "Marmaduke",
+    year: 2010,
+    character: "Marmaduke",
+  },
+  {
+    movie: "Little Fockers",
+    year: 2010,
+    character: "Kevin Rawley",
+  },
+  {
+    movie: "Hall Pass",
+    year: 2011,
+    character: "Rick Mills",
+  },
+  {
+    movie: "Midnight in Paris",
+    year: 2011,
+    character: "Gil",
+  },
+  {
+    movie: "Cars 2",
+    year: 2011,
+    character: "Lightning McQueen",
+  },
+  {
+    movie: "The Big Year",
+    year: 2011,
+    character: "Kenny Bostick",
+  },
+  {
+    movie: "The Internship",
+    year: 2013,
+    character: "Nick Campbell",
+  },
+  {
+    movie: "Are You Here",
+    year: 2013,
+    character: "Steve Dallas",
+  },
+  {
+    movie: "Free Birds",
+    year: 2013,
+    character: "Reggie",
+  },
+  {
+    movie: "Night at the Museum: Secret of the Tomb",
+    year: 2014,
+    character: "Jedediah Smith",
+  },
+  {
+    movie: "No Escape",
+    year: 2015,
+    character: "Jack Dwyer",
+  },
+];
+
+module.exports = { wowArr };
