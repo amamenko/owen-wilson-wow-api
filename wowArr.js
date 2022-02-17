12548b76bffd01c63cc779a33e9c3bc9bd9e056e
amamenko <amamenko@hotmail.com>
2022-02-16 23:35:22 -0500

    Added 'Bottle Rocket,' 'Breakfast of Champions,' and 'The Haunting' wow details

--- wowArr.js
+++ wowArr.js
@@ -3,18 +3,130 @@ const wowArr = [
     movie: "Bottle Rocket",
     year: 1996,
     character: "Dignan",
+    timestamp: "01:04:44",
+    full_line: "Wow. Wow. Okay, no more fighting.",
+    current_wow_in_movie: 1,
+    total_wows_in_movie: 3,
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/7spLWmHyAUz0TN8oNsAf5/8e9bc1adc817018f50bd4fc6bd4773ef/Bottle_Rocket_Wow_1_1080p.mp4",
+      "720p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/7AUtahGti2EMClGmW1rrZP/17b5e0650fe5d2aec31ccf3e205edcf9/Bottle_Rocket_Wow_1_720p.mp4",
+      "480p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/70L3BdbdbyszxEzg1BGAYN/135f1fc2c879f729997763981e00bc8f/Bottle_Rocket_Wow_1_480p.mp4",
+      "360p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/4Z8ngFYNxK8p8Zu78cldCM/1b221ed46312c9214b853fb514005430/Bottle_Rocket_Wow_1_360p.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/bs8ntwkklfua/4vqXtu1j5ruJiGhk6bBltj/bffee6dc120960546d91194fc7f99008/Bottle_Rocket_Wow_1.mp3",
+  },
+  {
+    movie: "Bottle Rocket",
+    year: 1996,
+    character: "Dignan",
+    timestamp: "01:04:45",
+    full_line: "Wow. Wow. Okay, no more fighting.",
+    current_wow_in_movie: 2,
+    total_wows_in_movie: 3,
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/5pEQrv5UtbI4Ad5vpJmjCx/880bd1e8342fc3b53f7142014fc00dac/Bottle_Rocket_Wow_2_1080p.mp4",
+      "720p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/26ZLeqKkZ6Ryt263GS3fAt/afab3530e5ef901592deae1007ee7eea/Bottle_Rocket_Wow_2_720p.mp4",
+      "480p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/12gYyFj41c0Gk7RehZYUDy/4527114e4d74a7edfe0ea4aae1b70a9e/Bottle_Rocket_Wow_2_480p.mp4",
+      "360p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/5a9FGGG1uVnVTFwvpx7jPs/b6cfec2b8a7c807399881bc838711953/Bottle_Rocket_Wow_2_360p.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/bs8ntwkklfua/7GFDsVHDOZcss2n2d9R65i/4d8cc87d144375c38e0f9752b915347b/Bottle_Rocket_Wow_2.mp3",
+  },
+  {
+    movie: "Bottle Rocket",
+    year: 1996,
+    character: "Dignan",
     timestamp: "01:08:25",
-    full_line: "Oh wow, that's one of your little drawings",
+    full_line: "Oh wow, that's one of your little drawings.",
+    current_wow_in_movie: 3,
+    total_wows_in_movie: 3,
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/5w3bsLhy7LFBcnxz90krH7/2998a0aa6c211656f7c0779e1cb8f4b6/Bottle_Rocket_Wow_3_1080p.mp4",
+      "720p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/ZVi6OFIq43MdyeZj2cEJ7/032182d10177e4e8695f37ee9b590554/Bottle_Rocket_Wow_3_720p.mp4",
+      "480p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/1PE1waVjCGjT8nQVYpJHE9/d93eeee42c4b8f5898c35f96550d8918/Bottle_Rocket_Wow_3_480p.mp4",
+      "360p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/5PBCBeixxOCpfo4IUA8oIw/cf8f69556b31b04a00d94fcad316f230/Bottle_Rocket_Wow_3_360p.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/bs8ntwkklfua/5xUOifToSk7ri9HUUZaPm4/ed39d2f462d53b97f10a57088a737871/Bottle_Rocket_Wow_3.mp3",
   },
   {
     movie: "Breakfast of Champions",
     year: 1999,
     character: "Monte Rapid",
+    timestamp: "00:52:59",
+    full_line: "Kilgore Trout. Wow.",
+    current_wow_in_movie: 1,
+    total_wows_in_movie: 1,
+    video: {
+      "1080p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/3XRms3VUiccrIuMa9IPvI2/c8e072c07ced11cc8eca854379abac35/Breakfast_of_Champions_1080p.mp4",
+      "720p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/1XtsYSf9Kslhb9Ar8SmQHw/62e10025f3e21c08778a6f1444d4b58c/Breakfast_of_Champions_720p.mp4",
+      "480p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/14Gt7a5PY24GZXeEUoOaIT/dc84d815d786d21b67e6bc26d076273a/Breakfast_of_Champions_480p.mp4",
+      "360p":
+        "https://videos.ctfassets.net/bs8ntwkklfua/1FmEgg2L7pWdbBo6pSJwZK/0309aba43e531159db02c38932318785/Breakfast_of_Champions_360p.mp4",
+    },
+    audio:
+      "https://assets.ctfassets.net/bs8ntwkklfua/1KRkfdHn1wFNjk7PqiG8me/d2cd53c422fcfbb2de87f845b65400bc/Breakfast_of_Champions.mp3",
   },
   {
     movie: "The Haunting",
     year: 1999,
     character: "Luke Sanderson",
+    timestamp: "00:23:20",
+    full_line: "Okay, obsessive indecision. Wow.",
+    current_wow_in_movie: 1,
+    total_wows_in_movie: 5,
+  },
+  {
+    movie: "The Haunting",
+    year: 1999,
+    character: "Luke Sanderson",
+    timestamp: "00:23:30",
+    full_line: "Wow, you're so dominant.",
+    current_wow_in_movie: 2,
+    total_wows_in_movie: 5,
+  },
+  {
+    movie: "The Haunting",
+    year: 1999,
+    character: "Luke Sanderson",
+    timestamp: "00:25:50",
+    full_line: "Wow.",
+    current_wow_in_movie: 3,
+    total_wows_in_movie: 5,
+  },
+  {
+    movie: "The Haunting",
+    year: 1999,
+    character: "Luke Sanderson",
+    timestamp: "00:41:34",
+    full_line: "Wow, I sorta got screwed on the ol' bedroom selection.",
+    current_wow_in_movie: 4,
+    total_wows_in_movie: 5,
+  },
+  {
+    movie: "The Haunting",
+    year: 1999,
+    character: "Luke Sanderson",
+    timestamp: "00:47:26",
+    full_line: "Yeah, no kidding. Did you see what she had on yesterday? Wow.",
+    current_wow_in_movie: 5,
+    total_wows_in_movie: 5,
   },
   {
     movie: "Shanghai Noon",
