6277feb01f7f38e60e83fa3e66cadc923567ad8e
amamenko <amamenko@hotmail.com>
2022-03-05 15:33:20 -0500

    Added sorting functionality and increased total number of results

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/wows/random", (req, res) => {
-  const numResults = Number(req.query.results);
+  const numResults = Number(req.query.results)
+    ? Number(req.query.results)
+    : typeof req.query.results === "string"
+    ? req.query.results.toLowerCase() === "all"
+      ? "all"
+      : ""
+    : "";
   const year = Number(req.query.year);
   const numberCurrentWow = Number(req.query.wow_in_movie);
   const movieName = req.query.movie;
   const directorName = req.query.director;
+  const sortBy = req.query.sort;
+  const direction = req.query.direction;
 
   let viableWows = wowArr;
 
@@ -41,10 +49,55 @@ app.get("/wows/random", (req, res) => {
     );
   }
 
-  const randomWow = sampleSize(
-    viableWows,
-    numResults ? (numResults >= 50 ? 50 : numResults) : 1
-  );
+  let randomWow = sampleSize(viableWows, numResults ? numResults : 1);
+
+  if (sortBy) {
+    if (sortBy === "movie") {
+      if (direction === "desc") {
+        randomWow = randomWow.sort((a, b) => b.movie.localeCompare(a.movie));
+      } else {
+        randomWow = randomWow.sort((a, b) => a.movie.localeCompare(b.movie));
+      }
+    } else if (sortBy === "release_date") {
+      if (direction === "desc") {
+        randomWow = randomWow.sort((a, b) =>
+          b.release_date.localeCompare(a.release_date)
+        );
+      } else {
+        randomWow = randomWow.sort((a, b) =>
+          a.release_date.localeCompare(b.release_date)
+        );
+      }
+    } else if (sortBy === "year") {
+      if (direction === "desc") {
+        randomWow = randomWow.sort((a, b) => b.year - a.year);
+      } else {
+        randomWow = randomWow.sort((a, b) => a.year - b.year);
+      }
+    } else if (sortBy === "director") {
+      if (direction === "desc") {
+        randomWow = randomWow.sort((a, b) =>
+          b.director.localeCompare(a.director)
+        );
+      } else {
+        randomWow = randomWow.sort((a, b) =>
+          a.director.localeCompare(b.director)
+        );
+      }
+    } else {
+      if (sortBy === "number_current_wow") {
+        if (direction === "desc") {
+          randomWow = randomWow.sort(
+            (a, b) => b.current_wow_in_movie - a.current_wow_in_movie
+          );
+        } else {
+          randomWow = randomWow.sort(
+            (a, b) => a.current_wow_in_movie - b.current_wow_in_movie
+          );
+        }
+      }
+    }
+  }
 
   res.send(randomWow);
   return;
@@ -67,10 +120,7 @@ app.get("/wows/ordered/:index?", (req, res) => {
         const secondNum = Number(splitIndeces[1]);
 
         if ((firstNum || firstNum === 0) && (secondNum || secondNum === 0)) {
-          const result = wowArr.slice(
-            firstNum,
-            Math.abs(secondNum - firstNum) >= 50 ? firstNum + 50 : secondNum + 1
-          );
+          const result = wowArr.slice(firstNum, secondNum + 1);
 
           res.send(result);
         } else {
